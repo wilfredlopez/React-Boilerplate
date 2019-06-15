3aa3f9cc92464c5e93977208a96914e1a7d3a4dc
unknown <cubamc@gmail.com>
2019-06-15 17:12:27 -0400

    ready

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Included to the package boilerplate
+
+React-router-dom, React-redux(with Easy-peasy) and React-Strap
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <div>
             <h1>About</h1>
             <p>Boilerplate Combining Create-react-app, React-router-dom, React-redux(with Easy-peasy) and React-Strap.</p>
-            <p><a href="https://github.com/wilfredlopez">Github - Wilfred Lopez @2019</a></p>
+            <p><a href="https://github.com/wilfredlopez/React-Boilerplate">Github - Wilfred Lopez @2019</a></p>
         </div>
     )
 }
